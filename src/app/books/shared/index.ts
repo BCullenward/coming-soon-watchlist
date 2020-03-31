41be0d66c887ee165724f6b1517490aff2819230
Brent Cullenward <bcullenward@gmail.com>
2020-03-31 08:32:24 -0500

    break out ISBNs

--- src/app/books/shared/index.ts
+++ src/app/books/shared/index.ts
@@ -1,3 +1,5 @@
 export * from './book.service';
 export * from './book-interface.model';
 export * from './restricted-words.validator';
+export * from './specific-length.validator';
+export * from './one-required.validator';
