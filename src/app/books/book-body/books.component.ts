e837f9705dee873142cb4f66f58ba8273bc28201
Brent Cullenward <bcullenward@gmail.com>
2020-04-08 14:16:54 -0500

    book pagination fixes (size not working)

--- src/app/books/book-body/books.component.ts
+++ src/app/books/book-body/books.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+// Server pagination example from michaelbromely.github.io/ngx-pagination/#/server-paging
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { ToastrService } from '../../common/toastr.service';
 import { ActivatedRoute } from '@angular/router';
 import { IBook } from '../shared/index';
@@ -19,18 +20,15 @@ interface IServerResponse {
 })
 
 export class BooksComponent implements OnInit {
-  //@Input('data')
   books: IBook[] = [];
-
   asyncBooks: Observable<IBook[]>;
   p: number = 1;
   total: number;
   loading: boolean;
   currentBooksToShow = [];
-  //defaultRecords: number = 10;
 
-  // MatPaginator Inputs
-  length = 100;
+  //// MatPaginator Inputs
+  //length = 100;
   pageSize = 10;
   pageSizeOptions = [5, 10, 25, 100];
   pageEvent: PageEvent;
@@ -41,35 +39,21 @@ export class BooksComponent implements OnInit {
     private route: ActivatedRoute) {
   }
 
+
   ngOnInit() {
-    //this.books = this.route.snapshot.data['books'];
-    //this.getPage(1);
-    this.getBooks();
-    //this.onPageChange(1);
-    console.log("current books 2: ", this.currentBooksToShow);
-  }
-
-  setPageSizeOptions(setPageSizeOptionsInput: string) {
-    this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
-  }
-
-
-  getBooks() {
     this.loading = true;
-    this.books = this.route.snapshot.data['books'];
+    this.getAllBooks();
     this.getPage(1);
 
+   // this.getPage(1);
     //this.currentBooksToShow = this.books.slice(0, this.pageSize);
-
     //console.log("current books: ", this.currentBooksToShow);
-  }
 
-  onPageChange($event) {
-    this.currentBooksToShow = this.books.slice($event.pageIndex * $event.pageSize, $event.pageIndex * $event.pageSize + $event.pageSize);
-  }
-
-  onPaginateChange(data) {
-    this.currentBooksToShow = this.books.slice(0, data.pageSize);
+    //this.books = this.route.snapshot.data['books'];
+    //this.getPage(1);
+    //this.getBooks();
+    //this.onPageChange(1);
+    //console.log("current books 2: ", this.currentBooksToShow);
   }
 
   getPage(page: number) {
@@ -79,19 +63,56 @@ export class BooksComponent implements OnInit {
       tap(res => {
         this.total = res.total;
         this.p = page;
-        this.loading = false;
-      }),
-      map(res => res.items)
+      }), map(res => res.items)
     );
 
     console.log("total: ", this.total);
-
-    console.log("async: ", this.asyncBooks);
+    this.loading = false;
   }
+
+  getData(e) {
+    //console.log("e: ", e);
+    this.getPage(e.pageIndex + 1);
+  }
+
+
+  //onPageChange($event) {
+  //  this.currentBooksToShow = this.books.slice($event.pageIndex * $event.pageSize, $event.pageIndex * $event.pageSize + $event.pageSize);
+  //}
+
+  //onPaginateChange(data) {
+  //  this.currentBooksToShow = this.books.slice(0, data.pageSize);
+  //}
+
+  //getPage3(page: number) {
+  //  this.loading = true;
+
+  //  this.asyncBooks = serverCall(this.books, page).pipe(
+  //    tap(res => {
+  //      this.total = res.total;
+  //      this.p = page;
+  //      this.loading = false;
+  //    }),
+  //    map(res => res.items)
+  //  );
+
+
+  //  console.log("total: ", this.total);
+  //  console.log("async: ", this.asyncBooks);
+  //}
 
   handleThumbnailClick(bookTitle) {
     this.toastr.success(bookTitle);
   }
+
+  //setPageSizeOptions(setPageSizeOptionsInput: string) {
+  //  this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+  //}
+
+  getAllBooks() {
+    this.books = this.route.snapshot.data['books'];
+  }
+
 }
 
 /** Simulate async HTTP call with delayed observable **/
@@ -99,11 +120,11 @@ function serverCall(books: IBook[], page: number): Observable<IServerResponse> {
   const perPage = 10;
   const start = (page - 1) * perPage;
   const end = start + perPage;
-  const secs = 10 * 1000;
+  let secs: number = 10 * 1000;
 
-  console.log("list: ", books.slice(start, end));
+  //console.log("list: ", books.slice(start, end));
   return of({
     items: books.slice(start, end),
     total: books.length
-  }).pipe(delay(secs));
+  }).pipe(delay(1000));
 }
